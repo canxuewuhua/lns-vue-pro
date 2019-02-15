700afca6c73e2d376fd706a44e82aaf52e67b1d2
zhuyongqiang <zhuyongqiang@kingsoft.com>
2019-02-15 16:57:42 +0800

    增加一个HelloVue的vue页面

---
+++ src/components/day01/HelloVue.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-card>
+    <div id="vue">Hello Vue.js! {{ message }}</div>
+    <div>
+      <el-button type="primary" icon="el-icon-upload" style="margin-left: 0.1rem;margin-top: 0.3rem" @click="loadCollectionRecordList">搜索</el-button>
+    </div>
+  </el-card>
+</template>
+<script type="text/javascript">
+export default {
+  name: '',
+  data () {
+    return {
+      message: '啦啦啦啦啦'
+    }
+  },
+  mounted () {
+    // this.loadCollectionRecordList()
+  },
+
+  methods: {
+    loadCollectionRecordList () {
+      alert('123')
+    }
+  }
+}
+</script>
+
+<style type="text/css">
+  #vue{
+    color: green;
+    font-size: 28px;
+  }
+</style>
